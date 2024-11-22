f6bfedf735ecf89788e57f6df277bd47447f2e7e
automatizacomea <automatizacomea@gmail.com>
2024-11-22 12:01:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,8 +37,6 @@ document.getElementById('instanceForm').addEventListener('submit', async functio
                 document.getElementById('qrCodeContainer').appendChild(qrCodeImage);
                 connectionStatus.innerHTML = 'Instância criada. Aguardando conexão...';
                 startCountdown();
-
-                setTimeout(checkConnection, 5000);
             }
         } else {
             const errorMessage = await response.text();
@@ -54,36 +52,6 @@ document.getElementById('instanceForm').addEventListener('submit', async functio
     }
 });
 
-async function checkConnection() {
-    try {
-        const response = await fetch('https://serven8.automatizacomea.cloud/webhook-test/Criador_Instancia_0.6', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            if (data.connected === true) {
-                showSuccessMessage("Conexão estabelecida com sucesso!");
-            } else {
-                setTimeout(checkConnection, 5000);
-            }
-        } else {
-            console.error('Erro ao verificar conexão.');
-        }
-    } catch (error) {
-        console.error('Erro na comunicação com o servidor ao verificar a conexão:', error);
-    }
-}
-
-function showSuccessMessage(message) {
-    document.getElementById('qrCodeContainer').innerHTML = '<div class="alert alert-success">' + message + '</div>';
-    document.getElementById('connectionStatus').innerHTML = message;
-    document.getElementById('countdown').innerHTML = '';
-}
-
 function startCountdown() {
     let timeRemaining = 50;
     const countdownInterval = setInterval(() => {
@@ -92,9 +60,9 @@ function startCountdown() {
 
         if (timeRemaining <= 0) {
             clearInterval(countdownInterval);
-            document.getElementById('countdown').innerHTML = 'Tempo Esgotado.
-Caso não tenha conseguido ler o QR Code, solicite novamente,
-Caso já conseguiu, verifique a confirmação no seu WhatsApp.';
+            document.getElementById('countdown').innerHTML = 'Tempo Esgotado.<br>' +
+                'Caso não tenha conseguido ler o QR Code, solicite novamente.<br>' +
+                'Caso já conseguiu, verifique a confirmação no seu WhatsApp.';
             document.getElementById('qrCodeContainer').innerHTML = 'QR Code expirado';
         }
     }, 1000);
